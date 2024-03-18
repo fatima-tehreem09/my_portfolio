4cb3ff8c1d8bfbcafff964c0d6fbee778a86d330
fatima-tehreem09 <146054314+fatima-tehreem09@users.noreply.github.com>
2024-03-18 16:09:17 +0500

    configured initialization

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:my_portfolio/src/components/app.dart';
 
 void main() {
-  runApp(
-    const App(),
-  );
+  App.initialize();
+  runApp(const App());
 }
--- lib/src/components/app.dart
+++ lib/src/components/app.dart
@@ -1,10 +1,50 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
-class App extends StatelessWidget {
+import '../base/nav/routes.dart';
+
+class App extends StatefulWidget {
   const App({super.key});
 
+  static Future<void> initialize() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    AppRouter.appUrlStrategy();
+  }
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp.router(
+      // theme: data,
+      builder: (context, child) {
+        return MediaQuery(
+          data: MediaQuery.of(context).copyWith(
+            textScaler: TextScaler.linear(1.0),
+          ),
+          child: child!,
+        );
+      },
+      routerConfig: AppRouter.router,
+      title: 'myPortfolio',
+      debugShowCheckedModeBanner: false,
+      scrollBehavior: MyCustomScrollBehavior(),
+    );
   }
+}
+
+class MyCustomScrollBehavior extends MaterialScrollBehavior {
+  @override
+  Set<PointerDeviceKind> get dragDevices => {
+        PointerDeviceKind.touch,
+        PointerDeviceKind.mouse,
+        PointerDeviceKind.stylus,
+        PointerDeviceKind.trackpad,
+        PointerDeviceKind.invertedStylus,
+        PointerDeviceKind.unknown,
+      };
 }
